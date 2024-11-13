4deab60459e8119c06227c337b8c64c4e16b8393
Phuc Le <lenguyenminhphuc0706@gmail.com>
2024-11-13 23:13:25 +0700

    fix ip

--- src/main/java/com/admin/MainController.java
+++ src/main/java/com/admin/MainController.java
@@ -2,11 +2,18 @@ package com.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
     @GetMapping("")
     public String showHomePage() {
-        return "manageUsers";
+        return "home"; // This should correspond to home.html
+    }
+
+    @GetMapping("/page/{pageName}")
+    public String showPage(@PathVariable String pageName) {
+        return pageName; // This assumes that the `pageName` corresponds to the template file name
     }
 }
+
