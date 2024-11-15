efbf9f0c3196e02012e5361246fb8e381aba5f08
KhaiNg03 <2251120418@ut.edu.vn>
2024-11-16 05:33:48 +0700

    Da duoc nang cap

--- src/main/java/com/admin/exception/CustomerNotFoundException.java
+++ src/main/java/com/admin/exception/CustomerNotFoundException.java
@@ -1,6 +1,6 @@
 package com.admin.exception;
 
-public class CustomerNotFoundException extends RuntimeException {
+public class CustomerNotFoundException extends Throwable {
     public CustomerNotFoundException(String message) {
         super(message);
     }
