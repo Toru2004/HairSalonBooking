e6b7204a04bb11a66d89f189b30502508b553783
LuuTruongVan <luutruongvan1607@gmail.com>
2024-11-17 15:50:40 +0700

    edit booknow

--- src/main/java/com/admin/service/StylistService.java
+++ src/main/java/com/admin/service/StylistService.java
@@ -16,23 +16,15 @@ public class StylistService {
         @Autowired
         private StylistRepository stylistRepository;
 
-    public List<Stylist> getAllStylists() {
-        // Chuyển Iterable thành List
-        Iterable<Stylist> stylistIterable = stylistRepository.findAll();
-        return new ArrayList<>((Collection<? extends Stylist>) stylistIterable);
-    }
 
-<<<<<<< HEAD
-=======
+
     public List<Stylist> listAll() {
         return (List<Stylist>) stylistRepository.findAll();  // Kiểm tra lại phương thức này
     }
->>>>>>> 8d6cccb27b28e0af70355f90cfba6daa5b5f689d
 
 
-        public List<Stylist> listAll() {
-            return (List<Stylist>) stylistRepository.findAll();
-        }
+
+
 
         public void save(Stylist stylist) {
             stylistRepository.save(stylist);
